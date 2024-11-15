f3ce6d16278fe4ea847cd605e83df4e9684b842f
wsxfs <www.982545816@qq.com>
2024-11-16 00:04:01 +0800

    增加一个组件:英雄细节,待实现标签页功能

---
+++ src/views/ChampionDetail.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="champion-detail">
+    <h2>英雄详情页 - {{ championId }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const championId = ref(route.params.championId)
+</script>
