6ab260a7bff6222740e7232f3b3c7a9a4926bdef
wsxfs <www.982545816@qq.com>
2024-12-03 21:34:08 +0800

    删除无用的文件,编写部分call_back

--- server_app/lcu/lcu_seraphine/connector/utils.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/11/7 19:33
-# @Author  : GZA
-# @File    : utils.py.py
--- server_app/lcu/lcu_seraphine/events/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/11/7 19:27
-# @Author  : GZA
-# @File    : __init__.py
