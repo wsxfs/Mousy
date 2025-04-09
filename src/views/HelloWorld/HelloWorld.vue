9fc669252039cb1e2d2fffb6de40dfeb2a33cd88
wsxfs <www.982545816@qq.com>
2025-04-09 11:33:54 +0800

    修改helloworld界面为可折叠

--- src/views/HelloWorld/HelloWorld.vue
+++ src/views/HelloWorld/HelloWorld.vue
@@ -153,6 +153,9 @@ watch(() => wsStore.lcuConnected, (newConnected) => {
   }
 });
 
+// 添加折叠面板的激活状态
+const activeNames = ref<string[]>(['lcu-status', 'ws-status'])
+
 onMounted(() => {
   wsStore.connect(); // 连接 WebSocket
 });
@@ -164,177 +167,185 @@ onUnmounted(() => {
 
 <template>
   <div class="dashboard">
-
     <!-- LCU 连接状态卡片 -->
     <div class="status-card">
       <h2 class="card-title">LCU 连接状态</h2>
-      <p :class="['status-message', { 'connected': wsStore.lcuConnected }]">
-        {{ lcuStatus }}
-      </p>
-      
-      <div class="button-group">
-        <ElButton 
-          type="success" 
-          size="large" 
-          @click="connectLCU"
-          :disabled="wsStore.lcuConnected"
-        >
-          连接 LCU
-        </ElButton>
-        <ElButton 
-          type="danger" 
-          size="large" 
-          @click="disconnectLCU"
-          :disabled="!wsStore.lcuConnected"
-        >
-          断开 LCU
-        </ElButton>
-      </div>
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="连接状态" name="lcu-status">
+          <p :class="['status-message', { 'connected': wsStore.lcuConnected }]">
+            {{ lcuStatus }}
+          </p>
+          
+          <div class="button-group">
+            <ElButton 
+              type="success" 
+              size="large" 
+              @click="connectLCU"
+              :disabled="wsStore.lcuConnected"
+            >
+              连接 LCU
+            </ElButton>
+            <ElButton 
+              type="danger" 
+              size="large" 
+              @click="disconnectLCU"
+              :disabled="!wsStore.lcuConnected"
+            >
+              断开 LCU
+            </ElButton>
+          </div>
 
-      <!-- 玩家信息卡片 -->
-      <div v-if="wsStore.lcuConnected && playerName" class="player-card">
-        <p class="player-name">{{ playerName }}</p>
-        <p v-if="playerId" class="player-tag">#{{ playerId }}</p>
-      </div>
+          <!-- 玩家信息卡片 -->
+          <div v-if="wsStore.lcuConnected && playerName" class="player-card">
+            <p class="player-name">{{ playerName }}</p>
+            <p v-if="playerId" class="player-tag">#{{ playerId }}</p>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
     </div>
 
     <!-- WebSocket 状态卡片 -->
     <div class="status-card">
-      <h2 class="card-title">WebSocket 状态</h2>
-      <p :class="['status-message', { 'connected': wsStore.isConnected }]">
-        {{ wsStore.isConnected ? 'WebSocket 已连接' : 'WebSocket 未连接' }}
-      </p>
-      
-      <div class="button-group">
-        <ElButton 
-          type="success" 
-          size="large" 
-          @click="wsStore.connect"
-          :disabled="wsStore.isConnected"
-        >
-          连接 WebSocket
-        </ElButton>
-        <ElButton 
-          type="danger" 
-          size="large" 
-          @click="wsStore.disconnect"
-          :disabled="!wsStore.isConnected"
-        >
-          断开 WebSocket
-        </ElButton>
-      </div>
-
-      <!-- 添加游戏状态显示 -->
-      <div class="game-state">
-        <h3>当前游戏状态</h3>
-        <p :class="['status-message', { 'connected': wsStore.isConnected }]">
-          {{ wsStore.gameState }}
-        </p>
-
-        <!-- 添加队伍 PUUID 列表显示 -->
-        <div class="team-puuid-info">
-          <h4>队伍 PUUID 列表</h4>
+      <h2 class="card-title">前后端 WebSocket 连接状态</h2>
+      <el-collapse v-model="activeNames">
+        <el-collapse-item title="连接状态" name="ws-status">
+          <p :class="['status-message', { 'connected': wsStore.isConnected }]">
+            {{ wsStore.isConnected ? 'WebSocket 已连接' : 'WebSocket 未连接' }}
+          </p>
           
-          <!-- 我方队伍 -->
-          <div class="team-section">
-            <h5>我方队伍</h5>
-            <div v-if="wsStore.syncFrontData.my_team_puuid_list?.length" class="puuid-list">
-              <div v-for="(puuid, index) in wsStore.syncFrontData.my_team_puuid_list" 
-                   :key="index" 
-                   class="puuid-item">
-                {{ puuid }}
+          <div class="button-group">
+            <ElButton 
+              type="success" 
+              size="large" 
+              @click="wsStore.connect"
+              :disabled="wsStore.isConnected"
+            >
+              连接 WebSocket
+            </ElButton>
+            <ElButton 
+              type="danger" 
+              size="large" 
+              @click="wsStore.disconnect"
+              :disabled="!wsStore.isConnected"
+            >
+              断开 WebSocket
+            </ElButton>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item title="游戏状态" name="game-state">
+          <div class="game-state">
+            <h3>当前游戏状态</h3>
+            <p :class="['status-message', { 'connected': wsStore.isConnected }]">
+              {{ wsStore.gameState }}
+            </p>
+
+            <!-- 添加队伍 PUUID 列表显示 -->
+            <div class="team-puuid-info">
+              <h4>队伍 PUUID 列表</h4>
+              
+              <!-- 我方队伍 -->
+              <div class="team-section">
+                <h5>我方队伍</h5>
+                <div v-if="wsStore.syncFrontData.my_team_puuid_list?.length" class="puuid-list">
+                  <div v-for="(puuid, index) in wsStore.syncFrontData.my_team_puuid_list" 
+                       :key="index" 
+                       class="puuid-item">
+                    {{ puuid }}
+                  </div>
+                </div>
+                <div v-else class="no-data">暂无我方队伍数据</div>
+              </div>
+
+              <!-- 敌方队伍 -->
+              <div class="team-section">
+                <h5>敌方队伍</h5>
+                <div v-if="wsStore.syncFrontData.their_team_puuid_list?.length" class="puuid-list">
+                  <div v-for="(puuid, index) in wsStore.syncFrontData.their_team_puuid_list" 
+                       :key="index" 
+                       class="puuid-item">
+                    {{ puuid }}
+                  </div>
+                </div>
+                <div v-else class="no-data">暂无敌方队伍数据</div>
               </div>
             </div>
-            <div v-else class="no-data">暂无我方队伍数据</div>
           </div>
+        </el-collapse-item>
 
-          <!-- 敌方队伍 -->
-          <div class="team-section">
-            <h5>敌方队伍</h5>
-            <div v-if="wsStore.syncFrontData.their_team_puuid_list?.length" class="puuid-list">
-              <div v-for="(puuid, index) in wsStore.syncFrontData.their_team_puuid_list" 
-                   :key="index" 
-                   class="puuid-item">
-                {{ puuid }}
+        <el-collapse-item title="选择英雄" name="champ-select" v-if="wsStore.gameState === '选择英雄'">
+          <div class="champ-select-info">
+            <div class="champ-info">
+              <!-- 候选席英雄显示 -->
+              <div class="bench-champs">
+                <h4>候选席英雄</h4>
+                <div v-if="Array.isArray(wsStore.syncFrontData.bench_champions) && wsStore.syncFrontData.bench_champions.length > 0" class="bench-list">
+                  <div v-for="championId in wsStore.syncFrontData.bench_champions" 
+                       :key="championId" 
+                       class="bench-item">
+                    <img 
+                      :src="getResourceUrl(championId)" 
+                      :alt="'Champion ' + championId"
+                      class="champion-icon"
+                    />
+                    <span>ID: {{ championId }}</span>
+                  </div>
+                </div>
+                <span v-else class="no-champ-info">无候选席英雄</span>
               </div>
-            </div>
-            <div v-else class="no-data">暂无敌方队伍数据</div>
-          </div>
-        </div>
-
-        <!-- 原有的选择英雄状态显示部分 -->
-        <div v-if="wsStore.gameState === '选择英雄'" class="champ-select-info">
-          <h3>选择英雄阶段</h3>
-          <div class="champ-info">
-            <!-- 候选席英雄显示 -->
-            <div class="bench-champs">
-              <h4>候选席英雄</h4>
-              <div v-if="Array.isArray(wsStore.syncFrontData.bench_champions) && wsStore.syncFrontData.bench_champions.length > 0" class="bench-list">
-                <div v-for="championId in wsStore.syncFrontData.bench_champions" 
-                     :key="championId" 
-                     class="bench-item">
+              
+              <!-- 当前英雄显示 -->
+              <div class="current-champ">
+                <h4>当前英雄</h4>
+                <template v-if="wsStore.syncFrontData.current_champion">
                   <img 
-                    :src="getResourceUrl(championId)" 
-                    :alt="'Champion ' + championId"
+                    :src="getResourceUrl(wsStore.syncFrontData.current_champion)" 
+                    :alt="'Champion ' + wsStore.syncFrontData.current_champion"
                     class="champion-icon"
                   />
-                  <span>ID: {{ championId }}</span>
-                </div>
+                  <span>ID: {{ wsStore.syncFrontData.current_champion }}</span>
+                </template>
+                <span v-else class="no-champ-info">未选择英雄</span>
               </div>
-              <span v-else class="no-champ-info">无候选席英雄</span>
+            </div>
+          </div>
+        </el-collapse-item>
+
+        <el-collapse-item title="消息历史" name="message-history">
+          <div class="message-history">
+            <div class="message-header">
+              <div class="message-controls">
+                <ElButton type="primary" size="small" @click="showMessages = !showMessages">
+                  {{ showMessages ? '隐藏消息' : '显示消息' }}
+                </ElButton>
+                <ElButton type="warning" size="small" @click="clearMessages">
+                  清空消息
+                </ElButton>
+              </div>
             </div>
             
-            <!-- 当前英雄显示 -->
-            <div class="current-champ">
-              <h4>当前英雄</h4>
-              <template v-if="wsStore.syncFrontData.current_champion">
-                <img 
-                  :src="getResourceUrl(wsStore.syncFrontData.current_champion)" 
-                  :alt="'Champion ' + wsStore.syncFrontData.current_champion"
-                  class="champion-icon"
-                />
-                <span>ID: {{ wsStore.syncFrontData.current_champion }}</span>
-              </template>
-              <span v-else class="no-champ-info">未选择英雄</span>
+            <div v-show="showMessages" class="message-list">
+              <div v-if="wsStore.messages.length === 0" class="no-messages">
+                暂无消息
+              </div>
+              <div v-else v-for="(msg, index) in wsStore.messages" :key="index" class="message-item">
+                <span class="message-time">{{ msg.timestamp }}</span>
+                <span class="message-content">{{ JSON.stringify(msg.content) }}</span>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
 
-      <!-- 消息历史记录 -->
-      <div class="message-history">
-        <div class="message-header">
-          <h3>消息历史</h3>
-          <div class="message-controls">
-            <ElButton type="primary" size="small" @click="showMessages = !showMessages">
-              {{ showMessages ? '隐藏消息' : '显示消息' }}
-            </ElButton>
-            <ElButton type="warning" size="small" @click="clearMessages">
-              清空消息
-            </ElButton>
+          <!-- 消息发送功能 -->
+          <div class="message-send">
+            <input 
+              v-model="messageToSend" 
+              placeholder="输入消息" 
+              @keyup.enter="sendMessage"
+            />
+            <ElButton type="primary" @click="sendMessage">发送消息</ElButton>
           </div>
-        </div>
-        
-        <div v-show="showMessages" class="message-list">
-          <div v-if="wsStore.messages.length === 0" class="no-messages">
-            暂无消息
-          </div>
-          <div v-else v-for="(msg, index) in wsStore.messages" :key="index" class="message-item">
-            <span class="message-time">{{ msg.timestamp }}</span>
-            <span class="message-content">{{ JSON.stringify(msg.content) }}</span>
-          </div>
-        </div>
-      </div>
-
-      <!-- 消息发送功能 -->
-      <div class="message-send">
-        <input 
-          v-model="messageToSend" 
-          placeholder="输入消息" 
-          @keyup.enter="sendMessage"
-        />
-        <ElButton type="primary" @click="sendMessage">发送消息</ElButton>
-      </div>
+        </el-collapse-item>
+      </el-collapse>
     </div>
   </div>
 </template>
@@ -351,7 +362,7 @@ onUnmounted(() => {
 .status-card {
   background: white;
   border-radius: 8px;
-  padding: 1.5rem;
+  padding: 1rem;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.08);
   text-align: center;
 }
@@ -410,22 +421,12 @@ onUnmounted(() => {
 }
 
 .message-history {
-  margin-top: 1rem;
-  border: 1px solid #eee;
-  border-radius: 4px;
-  padding: 1rem;
+  margin-top: 0;
+  border: none;
 }
 
 .message-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 1rem;
-}
-
-.message-header h3 {
-  margin: 0;
-  font-size: 1rem;
+  margin-bottom: 0.5rem;
 }
 
 .message-controls {
@@ -499,11 +500,9 @@ onUnmounted(() => {
 }
 
 .game-state {
-  margin-top: 1rem;
-  padding: 1rem;
-  background: #f8f9fa;
-  border-radius: 6px;
-  border: 1px solid #e9ecef;
+  margin-top: 0;
+  border: none;
+  padding: 0;
 }
 
 .game-state h3 {
@@ -513,11 +512,9 @@ onUnmounted(() => {
 }
 
 .champ-select-info {
-  margin-top: 1rem;
-  padding: 1rem;
-  background: #f8f9fa;
-  border-radius: 6px;
-  border: 1px solid #e9ecef;
+  margin-top: 0;
+  border: none;
+  padding: 0;
 }
 
 .champ-select-info h3, 
@@ -637,5 +634,45 @@ onUnmounted(() => {
   padding: 0.5rem;
   font-size: 0.9rem;
   font-style: italic;
+}
+
+/* 添加折叠面板样式 */
+:deep(.el-collapse-item__header) {
+  font-size: 1rem;
+  font-weight: 600;
+  color: #2c3e50;
+}
+
+:deep(.el-collapse-item__content) {
+  padding: 1rem;
+}
+
+/* 调整卡片内边距 */
+.status-card {
+  padding: 1rem;
+}
+
+/* 调整消息历史样式 */
+.message-history {
+  margin-top: 0;
+  border: none;
+}
+
+.message-header {
+  margin-bottom: 0.5rem;
+}
+
+/* 调整游戏状态样式 */
+.game-state {
+  margin-top: 0;
+  border: none;
+  padding: 0;
+}
+
+/* 调整选择英雄样式 */
+.champ-select-info {
+  margin-top: 0;
+  border: none;
+  padding: 0;
 }
 </style>
